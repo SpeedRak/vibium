9554314d530d2e1f9110b3b1eed1b6ae68ead786
SpeedRak <andrewchany2007@gmail.com>
2025-10-12 21:03:00 +0700

    Add files via upload

--- 15zadanie.py
+++ 15zadanie.py
@@ -2,7 +2,7 @@ def f(x, a):
     return (a % 9 == 0) and (  (280 % x == 0) <= ((a%x!=0)  <=  (730%x!=0))  )
 a = 1
 while True:
-    if all(for x in range(1, 1001) f(x, a)):
+    if all(f(x, a) for x in range(1, 1001) ):
         print(a)
         break
     a += 1
